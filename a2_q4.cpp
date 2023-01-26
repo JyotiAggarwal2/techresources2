3f45bbef64fd54c81b1b44829dad852279224d39
JyotiAggarwal2 <122285887+JyotiAggarwal2@users.noreply.github.com>
2023-01-26 14:55:03 +0530

    Submitting Ques 4 of Assignment 2

---
+++ a2_q4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter value of n"<<endl;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int space=1; space<=i-1; space++){
+            cout<<"  ";
+        }
+        for(int star=1; star<=n-i+1; star++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
